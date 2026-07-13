57642e9be1e0e6c392456c37716305b7f5d73153
school0107 <school010710@gmail.com>
2026-07-13 17:18:13 +0700

    Update SchoolItem.java

--- src/main/java/com/schoolitem/SchoolItem.java
+++ src/main/java/com/schoolitem/SchoolItem.java
@@ -2,6 +2,7 @@ package com.schoolitem;
 
 import com.schoolitem.commands.AddAbilityCommand;
 import com.schoolitem.commands.RemoveAbilityCommand;
+import com.schoolitem.config.PluginConfig;
 import com.schoolitem.listeners.DamageListener;
 import com.schoolitem.listeners.BlockBreakListener;
 import com.schoolitem.utils.VersionUtils;
@@ -9,10 +10,14 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class SchoolItem extends JavaPlugin {
     private static SchoolItem instance;
+    private PluginConfig pluginConfig;
 
     @Override
     public void onEnable() {
         instance = this;
+        
+        // Load config
+        pluginConfig = new PluginConfig(this);
         
         // Register commands
         getCommand("si").setExecutor(new AddAbilityCommand(this));
@@ -30,6 +35,7 @@ public class SchoolItem extends JavaPlugin {
         getLogger().info("§eSử dụng /si add <ability> <value> để thêm ability");
         getLogger().info("§eSử dụng /si remove để xóa ability");
         getLogger().info("§a=========================================");
+        getLogger().info("§7Config loaded: " + pluginConfig.getConfig().getKeys(false).size() + " sections");
     }
 
     @Override
@@ -37,7 +43,20 @@ public class SchoolItem extends JavaPlugin {
         getLogger().info("§cSchoolItem plugin đã được tắt!");
     }
 
+    @Override
+    public void reloadConfig() {
+        super.reloadConfig();
+        if (pluginConfig != null) {
+            pluginConfig.reloadConfig();
+        }
+        getLogger().info("§aConfig đã được reload!");
+    }
+
     public static SchoolItem getInstance() {
         return instance;
+    }
+
+    public PluginConfig getPluginConfig() {
+        return pluginConfig;
     }
 }
